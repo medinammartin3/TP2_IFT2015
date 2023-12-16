cfcd2bf981bf40d9047901aca553a4ea8627e4d3
Martin <medinammartin3@gmail.com>
2023-12-15 19:53:42 -0500

    Entry implementation

--- src/Entry.java
+++ src/Entry.java
@@ -1,4 +1,18 @@
-public interface Entry<K,V> {
-    K getKey(); // return the key stored in the Entry
-    V getValue(); // return the value stored in the Entry
+public class Entry<K,V>{
+    private K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
 }
+
