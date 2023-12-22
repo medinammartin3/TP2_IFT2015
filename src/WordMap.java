6b90a78d785b284cc5f19853f1ad032a4fcdcc07
Martin <medinammartin3@gmail.com>
2023-12-21 21:57:46 -0500

    .

--- src/WordMap.java
+++ src/WordMap.java
@@ -44,6 +44,8 @@ public class WordMap<K,V> implements Map<K,V>{
         }
         return null;
     }
+
+    // Eliminer l'element au complet
     public V remove( K key ) {
         int hashValue = hashValue(key);
         LinkedList<Entry<K,V>> bucket = table[hashValue];
@@ -59,34 +61,30 @@ public class WordMap<K,V> implements Map<K,V>{
         }
         return null;
     }
-    public V replace( K key, V value ) {
-        int hashValue = hashValue(key);
-        LinkedList<Entry<K,V>> bucket = table[hashValue];
-        if( bucket == null ) {
-            return null;
-        }
-        for (Entry<K, V> entry : bucket) {
-            if (entry.getKey().equals(key)){
-                entry.setValue(value);
-                return value;
-            }
-        }
-        return null;
-    }
 
     public V put( K key, V value ) {
         int hashValue = hashValue(key);
         LinkedList<Entry<K,V>> bucket = table[hashValue];
-        if( bucket == null )
+        if( bucket == null ) // Le bucket où on veut mettre le mot est vide
+        {
             bucket = table[hashValue] = new LinkedList<>();
-        int oldSize = bucket.size();
-        bucket.add(new Entry<>(key,value));
-        this.n += ( bucket.size() - oldSize ); // size may have increased
-        if (n > capacity * 0.75) // keep load factor <= 0.75
+        } else {
+            for (Entry<K, V> entry : bucket) {
+                if (entry.getKey().equals(key)) // La cle existe deja
+                {
+                    entry.setValue(value); // On change seulement la valeur
+                    return value;
+                }
+            }
+        }
+        bucket.add(new Entry<>(key,value)); // Si la cle n'existe pas, on ajoute un nouveau element
+        this.n += 1; // Mettre a jour le nombre d'elements
+        if (n > capacity * 0.75) // Maintenir le facteur de charge <= 0.75
             resize(2 * capacity + 1);
         return value;
     }
 
+    // ArrayList de toutes les cles de la Map
     public Iterable<K> keySet() {
         ArrayList<K> buffer = new ArrayList<>();
         for (LinkedList<Entry<K,V>> bucket : table) {
@@ -99,6 +97,7 @@ public class WordMap<K,V> implements Map<K,V>{
         return buffer;
     }
 
+    // ArrayList de tous les elements de la Map
     public Iterable<Entry<K,V>> entrySet() {
         ArrayList<Entry<K,V>> buffer = new ArrayList<>();
         for (LinkedList<Entry<K,V>> bucket : table) {
@@ -111,6 +110,7 @@ public class WordMap<K,V> implements Map<K,V>{
         return buffer;
     }
 
+    // ArrayList de toutes les valeur de la Map
     public Iterable<V> values() {
         ArrayList<V> buffer = new ArrayList<>();
         for (LinkedList<Entry<K,V>> bucket : table) {
@@ -123,7 +123,7 @@ public class WordMap<K,V> implements Map<K,V>{
         return buffer;
     }
 
-
+    // Code de hashage
     private int hashValue( K key ) {
         return Math.abs(key.hashCode()) % capacity;
     }
@@ -133,15 +133,15 @@ public class WordMap<K,V> implements Map<K,V>{
         for( Entry<K,V> entry : this.entrySet() )
             buffer.add( entry );
         this.capacity = newCapacity;
-        this.createTable(); // based on updated capacity
-        this.n = 0; // will be recomputed while reinserting entries
+        this.createTable(); // Avec la nouvelle capacite
+        this.n = 0; // Va augmenter automatiquement lors de l'insertion des elements
         for( Entry<K,V> entry : buffer )
             put( entry.getKey(), entry.getValue() );
     }
 
     @SuppressWarnings("unchecked")
     private void createTable(){
-        // create an empty table of current capacity
+        // Creer un nouveau tableau
         table = new LinkedList[this.capacity];
     }
 }
